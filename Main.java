2eb5c19560e12f405d79d3023173f5797ada76bf
MlKEWAZOWSKl <aclements996@gmail.com>
2015-11-30 23:05:05 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+
+public class gameMenu {
+	public static void main(String args[]){
+
+	JFrame menuFrame = new JFrame("Main Menu");
+
+
+	menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    menuFrame.setBounds(0,0,screenSize.width, screenSize.height);
+    JPanel backgroundPanel = new JPanel(new BorderLayout(screenSize.width, screenSize.height));
+    
+
+	backgroundPanel.setBackground(Color.RED);
+	backgroundPanel.setOpaque(true);
+	backgroundPanel.setVisible(true); 
+
+    menuFrame.setLocationRelativeTo(null);
+    menuFrame.setVisible(true);
 
